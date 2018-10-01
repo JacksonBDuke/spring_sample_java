39d3791d1311cab3e4fccad0c58d3416f3ab6a4e
Jackson Duke <JacksonBDuke@gmail.com>
2018-10-01 14:32:43 -0500

    Going through various injections: Setter, Constructor, Member.

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -11,12 +11,15 @@ import org.springframework.stereotype.Service;
 @Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-//	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
-	@Autowired
+    @Autowired
     private CustomerRepository customerRepository;
-	/* (non-Javadoc)
-	 * @see com.pluralsight.service.CustomerService#findAll()
-	 */
+
+    @Autowired
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        System.out.println("We're using setter injection.");
+        this.customerRepository = customerRepository;
+    }
+
 	@Override
 	public List<Customer> findAll() {
 		return customerRepository.findAll();
